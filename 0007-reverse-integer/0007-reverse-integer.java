7d3b29ef56e0ea3e3b9b4d97b621212e84250950
nishthasud <nishthasud@gmail.com>
2023-07-16 16:48:09 +0530

    Time: 23 ms (6.26%), Space: 40.8 MB (5.58%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int reverse(int x) {
+      int flag=0;
+      // track if input no is positive or negative
+      int absX=x;
+      if(x<0)
+      {
+        System.out.println("Negative");
+        if(Integer.MIN_VALUE==x)
+        return 0;
+        else
+        {  
+        absX=(x)*(-1);
+        System.out.println(absX);
+        flag=1;
+        }
+      }
+      
+      String a=String.valueOf(absX);
+      System.out.println(a);
+      char b[]=a.toCharArray();
+      int start=0,end=b.length-1;
+     while(start<=end)
+     {
+       char temp=b[start];
+       System.out.println("temp="+temp);
+      b[start]=b[end];
+       b[end]=temp;
+       start++;
+       end--;
+       
+       
+     }
+      String c=String.valueOf(b);
+      int i=0;
+      System.out.println("c="+c);
+     while(c.charAt(i)==0)
+     {
+      
+       i++;
+       
+     }
+      String temp="";
+     
+      temp=c.substring(i);
+      
+      if(flag!=1)
+      {
+        System.out.println(temp);
+        long test = Long.parseLong(temp);
+    if (test > Integer.MAX_VALUE )
+        
+    
+        return 0;
+        else 
+          return Integer.parseInt(temp);
+             
+      }
+      
+      else
+      {
+        long test = Long.parseLong(temp)*-1;
+        if(test< Integer.MIN_VALUE)
+          return 0;
+        else
+          return ((int)test);
+      }
+    }
+}
